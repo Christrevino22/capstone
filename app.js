e443f7a448fe86308f599d5113c3cc4f0d080d42
Christopher Trevino <chrisdadef@gmail.com>
2021-04-18 20:53:06 -0500

    need to restart

--- app.js
+++ app.js
@@ -1,38 +1,115 @@
-const express = require('express');
-const mysql = require('mysql');
+// NEED TO FIGURE OUT HOW TO LINK MY DATABASE UP TO MY SERVER BUT I ALSO NEED TO CREATE A SERVER TOO DUHHHH
 
-//CREATING THE CONNECTION
-const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  // password: '',
-  // database: 'nodemysql',+
-})
 
-//CONNECTING
-db.connect((err)=> {
-  if(err) {
-    throw err;
 
-  }
-  console.log('MYSQL CONNECTED.....')
-})
-const app = express()
 
-//CREATE DB
-app.get('/createdb', (req, res) => {
-  let sql = 'CREATE DATABASE nodemysql';
-  db.query(sql, (err, result) => {
-    if(err) throw err
-      console.log(result);
-      res.send('database created....');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const express = require('express');
+// const mysql = require('mysql');
+
+// //CREATING THE CONNECTION
+// const db = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   // password: '',
+//   // database: 'nodemysql',+
+// })
+
+// //CONNECTING
+// db.connect((err)=> {
+//   if(err) {
+//     throw err;
+
+//   }
+//   console.log('MYSQL CONNECTED.....')
+// })
+// const app = express()
+
+// // //CREATE DB
+// // app.get('/createdb', (req, res) => {
+// //   let sql = 'CREATE DATABASE nodemysql';
+// //   db.query(sql, (err, result) => {
+// //     if(err) throw err
+// //       console.log(result);
+// //       res.send('database created....');
   
-  })
+// //   })
 
-})
+// // })
 
 
-app.listen('3000', () => {
-    console.log('Server started on PORT 3000')
-});
+// app.listen('3000', () => {
+//     console.log('Server started on PORT 3000')
+// });
 
