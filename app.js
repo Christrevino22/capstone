a0c847a14b3a3d48f322faeef1f7c7fdade5a0e8
Christopher Trevino <chrisdadef@gmail.com>
2021-04-21 19:18:53 -0500

    lost messing with terminal

--- app.js
+++ app.js
@@ -24,6 +24,15 @@ app.get("/users", (req, res) => {
 
 app.listen(5000, () => console.log("Server started"));
 
+
+
+
+
+
+// vscode://vscode.github-authentication/did-authenticate?windowid=1&code=762cbc2e01bb606429ff&state=f989ba32-49e5-4172-9455-7ada942e783b
+
+
+
 // const express = require('express');
 // const mysql = require('mysql');
 
