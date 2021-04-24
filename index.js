3d51e3a9c2bd0def48b667572e3715255dbef2c0
Christopher Trevino <chrisdadef@gmail.com>
2021-04-24 11:18:17 -0500

    done for the night too sleepy

--- index.js
+++ index.js
@@ -7,21 +7,18 @@ const PORT = process.env.PORT || 5000;
 
  
 
-// db.connect();
+
 
 app.get("/", (req, res) => {
-  // const sql = "SELECT * FROM users";
 
-  // db.query(sql, (err, result) => {
-  //   if (err) throw err;
-  //   res.send(result);
   res.send('Hello Future Dev!!!!')
   // });
 });
 
 app.listen(PORT, () => console.log("Server started... Listening on PORT 5000"));
 
-// vscode://vscode.github-authentication/did-authenticate?windowid=1&code=762cbc2e01bb606429ff&state=f989ba32-49e5-4172-9455-7ada942e783b
+
+
 
 // const express = require('express');
 // const mysql = require('mysql');
--- sql/connection.js
+++ sql/connection.js
@@ -1,15 +1,17 @@
 const mysql = require("mysql");
 
+require('dotenv').config();
+
 class Connection {
   constructor() {
     if (!this.pool) {
       console.log("creating connection.....");
       this.pool = mysql.createPool({
         connectionLimit: 100,
-        host: process.env.HOST,
-        user: process.env.USER,
-        password: process.env.PASSWORD,
-        database: process.env.DATABASE,
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
       });
       return this.pool;
     }
@@ -18,4 +20,5 @@ class Connection {
 }
 
 const instance = new Connection();
+
 module.exports = instance;
